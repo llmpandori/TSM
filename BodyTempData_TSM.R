aa32224a308816010d69d22079d7edc82939f27e
llmpandori <lmcquinn@uci.edu>
2020-03-31 11:48:24 -0700

    Initial commit - figuring out GitHub in RStudio

---
+++ BodyTempData_TSM.R
@@ -0,0 +1,10 @@
+#################################################################
+#Title: Thermal Safety Margin Manuscript Data Analysis & Visualization
+#Purpose: (1) Document temperature at CCSP
+#         (2) Calculate LT50s for benthic mussel life stages
+#         (3) Body temp for benthic mussel life stages
+#         (4) TSM calculation
+#Created by: L Pandori
+#Created: 03/31/2020
+#Last edited: 03/31/2020
+################################################################
